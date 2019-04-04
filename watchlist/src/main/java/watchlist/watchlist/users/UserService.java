3a2e1440b00b51bd6ffea78c73f2cf76591558bf
JuniorBelinda <belinda.schuehle@nyp.ch>
2019-04-04 14:37:13 +0200

    Auto stash before cherrypick of "added deleteUser"

--- watchlist/src/main/java/watchlist/watchlist/users/UserService.java
+++ watchlist/src/main/java/watchlist/watchlist/users/UserService.java
@@ -1,14 +1,22 @@
 package watchlist.watchlist.users;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserServiceable {
 
 	private UserRepository repository;
+	
+	@Autowired
+	public UserService(UserRepository repository) {
+		this.repository = repository;
+	}
 	
 	@Override
 	public Optional<User> getById(Long id) {
@@ -22,15 +30,25 @@ public class UserService implements UserServiceable {
 
 	@Override
 	public void createUser(User user) {
-		repository.save(user);
+		this.repository.save(user);
 	}
 
 	@Override
 	public void updateUser(Long id, User body) {
 		var user = getById(id);
 
+		try {
+			user.get().setFirstname(body.getFirstname());
+			user.get().setLastName(body.getLastname());
+			user.get().setBirthdate(new SimpleDateFormat("yyyy-MM-dd").parse(String.valueOf(body.getBirthdate())));
+			user.get().setEmail(body.getEmail());
+			user.get().setUsername(body.getUsername());
+			user.get().setPassword(body.getPassword());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		
-		repository.save(body);
+		repository.save(user.get());
 	}
 
 	@Override
