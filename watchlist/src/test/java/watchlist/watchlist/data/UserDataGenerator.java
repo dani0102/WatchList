c7cd19f4d9e453bccd71b3c7b62014eebf285c70
JuniorBelinda <belinda@schuehle.ch>
2019-04-09 16:30:29 +0200

    cleanup, added: TODOs
    TODO: UserDataGenerator - Change Date-format
    TODO: RoleDataGenerator - Fix class setup, if fixed re-imlement into DataGenerators

--- watchlist/src/test/java/watchlist/watchlist/data/UserDataGenerator.java
+++ watchlist/src/test/java/watchlist/watchlist/data/UserDataGenerator.java
@@ -14,6 +14,7 @@ public class UserDataGenerator implements DataGeneratable<User>{
 	public DataHolder<User> generate() {
 		
 		return DataHolder.of(
+				// TODO: Not deprecated version of Date
 				new User(0L, "Daniela", "Simoes", new Date(2001, 6, 16), "danielamsimoes01@gmail.com", "test0102", "test0102password"),
 				new User(1L, "Belinda", "Stifani", new Date(1997, 10, 23), "belinda.stifani@gmail.com", "bruce0102", "bruce0102password"),
 				new User(2L, "Belinda", "Schuehle", new Date(2001, 4, 16), "beli.schuehle@gmail.com", "dabi0102", "dabi0102password")
